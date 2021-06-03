122fdf5c6ef6216f5423cb28c6ed81f81ec62198
RikoBgrff <riko.bagirli777@gmail.com>
2021-06-03 23:21:45 +0400

    Add project files.

---
+++ NotepadMementoPattern/Commands/UndoCommand.cs
@@ -0,0 +1,19 @@
+﻿using System.Windows;
+using NotepadMementoPattern.Entities;
+
+namespace NotepadMementoPattern.Commands
+{
+    public class UndoCommand : CommandBase
+    {
+        public UndoCommand(History history) : base(history)
+        {
+        }
+
+        public override bool CanExecute(object parameter) => _history.CanBack;
+
+        public override void Execute(object parameter)
+        {
+            _history.Back();
+        }
+    }
+}
---
+++ NotepadMementoPattern/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using NotepadMementoPattern.Commands;
+using NotepadMementoPattern.Entities;
+
+
+namespace NotepadMementoPattern.ViewModels
+{
+    public class MainViewModel:BaseViewModel
+    {
+        public Editor Editor { get; set; }
+        private History _history;
+
+        public SaveCommand SaveCommand { get; set; }
+        public UndoCommand UndoCommand { get; set; }
+        public CommandBase RedoCommand { get; set; }
+
+        public MainViewModel()
+        {
+            Editor = new Editor();
+            _history = new History(Editor);
+
+            SaveCommand = new SaveCommand(_history);
+            UndoCommand = new UndoCommand(_history);
+            RedoCommand = new RedoCommand(_history);
+        }
+    }
+}
